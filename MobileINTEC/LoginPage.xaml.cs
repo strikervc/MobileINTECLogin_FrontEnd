0fa26f7569c15bca3e02a8d7ed6d9288df8a3a64
Victor Valdez <victor334_@hotmail.com>
2021-02-19 16:13:46 -0400

    Mobile Login Project uploaded correctly

---
+++ MobileINTEC/LoginPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileINTEC
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : TabbedPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        async void GoToSign(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new RegPage());
+        }
+
+        async void LogInUser(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(User.Text) | string.IsNullOrEmpty(Password.Text))
+            {
+                await DisplayAlert("Error", "Campo email y/o contraseña no puede estar vacío", "Ok");
+            }
+            else
+            {
+                await DisplayAlert("Bienvenido", $"Hola {User.Text}", "Ok");  
+            }
+        }
+
+    }
+}
---
+++ MobileINTEC/RegPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileINTEC
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegPage : ContentPage
+    {
+        public RegPage()
+        {
+            InitializeComponent();
+        }
+
+        async void GoToMaster(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(Name.Text) | string.IsNullOrEmpty(Email.Text) | string.IsNullOrEmpty(Password.Text)| string.IsNullOrEmpty(Confirm.Text))
+            {
+                await DisplayAlert("Error", "Campo nombre, email y/o contraseña no puede estar vacío", "Ok");
+            }
+            else if (!Password.Text.Equals(Confirm.Text))
+            {
+                await DisplayAlert("Error", $"Las contraseñas deben coincidir", "Ok");
+            }
+            else
+            {
+                await Navigation.PushAsync(new HomePage());
+                await DisplayAlert("Bienvenido", $"Bienvenido {Name.Text}", "Ok");
+            }
+            
+        }
+    }
+}
